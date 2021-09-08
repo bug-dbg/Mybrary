71140d1bdc865224a3d358fdfdf69e4d9c6d3694
Ren <renactub@gmail.com>
2021-09-08 19:52:10 +0800

    Initial Setup

---
+++ server.js
@@ -0,0 +1,30 @@
+// check if were running in the production env or not
+// we need to not load this production env 
+// unless were in our development env
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+    // load of dotenv dependencies
+}
+
+const express = require('express')
+const app = express();
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+// exporting the database 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongdb'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
